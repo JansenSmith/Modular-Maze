4bc99568ec0af6d4174a0200e2c2b8ffc2d91222
janzen <janzen@zipidy>
2022-03-08 17:48:32 -0500

    Ignore the project files

--- Maze-Wall.groovy
+++ Maze-Wall.groovy
@@ -1,1 +1,12 @@
 //Your code here
+// Load first STL file from the git repo
+File servoFile = ScriptingEngine.fileFromGit(
+	"https://github.com/WPIRoboticsEngineering/Modular-Maze.git",
+	"Maze_Wall_Post.stl");
+// Load the .CSG from the disk and cache it in memory
+CSG Maze_Wall_Post  = Vitamins.get(servoFile)
+
+// code goes here
+
+
+return Maze_Wall_Post
