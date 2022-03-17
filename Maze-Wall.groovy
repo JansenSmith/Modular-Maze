5a234a5a58dcd716b1b4a0ea0382f76e22f2384f
greg <greg@potato>
2022-03-17 16:03:37 -0400

    Finished refactoring
    Remade the base in bowler(gave up having only some corners round due to complexity)
    Parametrized dimensions
    addded parameters to github

--- Maze-Wall.groovy
+++ Maze-Wall.groovy
@@ -3,52 +3,82 @@ import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 
 import eu.mihosoft.vrl.v3d.CSG
 
-
-//Your code here
-// Load first STL file from the git repo
+// Load stls
 File servoFile = ScriptingEngine.fileFromGit(
 	"https://github.com/WPIRoboticsEngineering/Modular-Maze.git",
 	"Maze_Wall_Post.stl");
-// Load the .CSG from the disk and cache it in memory
-CSG Maze_Wall_Post  = Vitamins.get(servoFile)
+DEFAULT_POST  = Vitamins.get(servoFile)
 						.toXMin()
 						.toYMin()
-
-
 						
-// code goes here
-Maze_Wall_Post = Maze_Wall_Post.movex(-Maze_Wall_Post.getCenterX())
-Maze_Wall_Post = Maze_Wall_Post.movey(-Maze_Wall_Post.getCenterY())
+//Setup Params
+Post_Height 	= new LengthParameter("Post Height(in)",8,[24.0,1.0])
+Base_Size		= new LengthParameter("Base Size(mm)",80,[240.0,10.0])
+Base_Height	= new LengthParameter("Base Height(mm)",1,[20.0,0.1])
+Peg_Radius	= new LengthParameter("Peg Radius(mm)",2,[8.0,0.1])
 
-Maze_Wall_Peg = new RoundedCube(80,80,10).cornerRadius(2).toCSG()
+// Object Generation Functions
+CSG Make_Post(){
+	//Creates a centered version of the default post
+	Maze_Wall_Post = DEFAULT_POST.movex(-DEFAULT_POST.getCenterX())
+	Maze_Wall_Post = Maze_Wall_Post.movey(-Maze_Wall_Post.getCenterY())
+	Maze_Wall_Post = Maze_Wall_Post.movey(-Maze_Wall_Post.getMinZ())
+220
+	def current_height = Maze_Wall_Post.getTotalZ()//get post height
+	def desired_height = Post_Height.getMM() *25.4// get scale (Post_Height.getMM() returns a value in inches)
 
-def Post_Height 	= new LengthParameter("Height(in)",8,[24.0,1.0])
-def Base_Size		= new LengthParameter("Size(cm)",8,[24.0,1.0])
-def Base_Height	= new LengthParameter("Height(cm)",8,[24.0,1.0])
-//Peg size roughtly 4mm
-Maze_Wall_Post.setParameter(Post_Height)
-Maze_Wall_Peg.setParameter(Base_Size).setParameter(Base_Height)
+	//scales post
+	def scalar = desired_height / current_height
+	Maze_Wall_Post = Maze_Wall_Post.scalez(scalar)
+	
+	//attach params and set up regenerate function* 
+	//*(function used to regenerate the object when the params are changed, in this case this function)
+	return Maze_Wall_Post
+			.setParameter(Post_Height)
+			.setRegenerate({Make_Post()})
+}
 
-def current_height = Maze_Wall_Post.getTotalZ()
-def desired_height = Post_Height.getMM() *25.4
+CSG Make_Peg(){
+	//get parameter values
+	h = Base_Height.getMM()
+	w = Base_Size.getMM()
+	r = Peg_Radius.getMM()
+	//make a rounded cube
+	Maze_Wall_Peg = new RoundedCube(w,w,h).cornerRadius(2).toCSG()
+	//make a cylinder
+	Peg = new Cylinder(r,r,r*10,(int)30).toCSG()
+	//combine them, and translase below xy plane
+	Maze_Wall_Peg  = Maze_Wall_Peg.movez(-Maze_Wall_Peg.getMaxZ())
+	Peg = Peg.movez(-Maze_Wall_Peg.getMaxZ())
+	Maze_Wall_Peg  = Maze_Wall_Peg.union(Peg)
+	
 
-def scalar = desired_height / current_height
+	//attach params and regenerate function
+	return Maze_Wall_Peg
+			.setParameter(Base_Size)
+			.setParameter(Base_Height)
+			.setParameter(Peg_Radius)
+			.setRegenerate({Make_Peg()})
+	}
 
-Maze_Wall_Post = Maze_Wall_Post.scalez(scalar)
+//Object Generation
+Maze_Wall_Post = Make_Post()
+Maze_Wall_Peg = Make_Peg()
 
-
-
-//String filename =ScriptingEngine.getWorkspace().getAbsolutePath()+"/CopiedStl.stl";
-//FileUtil.write(Paths.get(filename),
-//		servo.toStlString());
-//println "STL EXPORT to "+filename
-//ScriptingEngine.pushCodeToGit("git@github.com:Gregory-Marshall/Modular-Maze.git",
-//	 "main", 
-//	 "Maze-Wall-Post-Printable.stl", 
-//	 Maze_Wall_Post.toStlString(), 
-//	 "Printable 11inch demo version")
-
+//Object Placement
+//BowlerStudioController.addCsg(Maze_Wall_Post.intersect(Maze_Wall_Peg)) //sanity check for peg size
 BowlerStudioController.addCsg(Maze_Wall_Post)
 BowlerStudioController.addCsg(Maze_Wall_Peg)
-BowlerKernel.speak("Build Completed")
+
+/* 
+String filename =ScriptingEngine.getWorkspace().getAbsolutePath()+"/CopiedStl.stl";
+FileUtil.write(Paths.get(filename),
+		servo.toStlString());
+println "STL EXPORT to "+filename
+ScriptingEngine.pushCodeToGit("https://github.com/WPIRoboticsEngineering/Modular-Maze.git",
+	 "main", 
+	 "Maze-Wall-Post-Printable.stl", 
+	 Maze_Wall_Post.toStlString(), 
+	 "Printable 11inch demo version")
+*/
 return null
